d4e1670a82314a718c71e58d78d6c163edf1d229
61911 <61911@Any3.com>
2018-04-27 09:06:40 +0800

    init demo project

---
+++ src/main/java/com/example/one/FootstoneApplication.java
@@ -0,0 +1,12 @@
+package com.example.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootstoneApplication.class, args);
+	}
+}
